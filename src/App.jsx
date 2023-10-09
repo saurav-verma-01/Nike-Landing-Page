9c359fee5951a0387afb0ee7732bf17605b12eaf
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-10-09 15:29:38 +0530

    tailwind css project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+// import React from "react";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="relative">
+      NAVBAR
+      <section className="xl:padding-1 wide:padding-r paddig-b">HERO</section>
+      <section className="padding">POPULAR PRODUCTS</section>
+      <section className="padding">SUPER QUALITY</section>
+      <section className="padding-x py-10">SERVICES </section>
+      <section className="padding">SPECIAL OFFERS</section>
+      <section className="padding">CUSTOMER REVIEWS</section>
+      <section className="padding"></section>
+      <section className="padding">SPECIAL OFFERS</section>
+    </main>
+  );
 };
 
 export default App;

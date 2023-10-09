b38ebd7a5ad2e4816a1d6fc7de1a4d30f7d8b1aa
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-10-09 22:09:07 +0530

    hero section part 2

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Button from "../components/Button";
 import { arrowRight } from "../assets/icons";
-import { statistics } from "../constants";
+import { shoes, statistics } from "../constants";
+import { bigShoe1 } from "../assets/images";
+import ShoeCard from "../components/ShoeCard";
 
 const Hero = () => {
   return (
@@ -39,6 +41,28 @@ const Hero = () => {
               <p className="leading-7 font-montserrat text-slate-gray">
                 {stat.label}
               </p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="relative flex justify-center items-center flex-1 xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+        <img
+          src={bigShoe1}
+          alt="big shoe  collection"
+          width={610}
+          height={500}
+          className="object-contain relative z-10"
+        />
+
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
+          {shoes.map((shoe) => (
+            <div key={shoe}>
+              <ShoeCard
+                imgURL={shoe}
+                changeBigShoeImage={() => {}}
+                bigShoeImage=""
+              />
             </div>
           ))}
         </div>

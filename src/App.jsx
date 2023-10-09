d98772f0465f68c540a697a0372c5f5d4b898400
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-10-09 20:04:42 +0530

    Navbar Structure Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,45 @@
 // import React from "react";
-
+import NavBar from "./components/NavBar";
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "./sections";
 const App = () => {
   return (
     <main className="relative">
-      NAVBAR
-      <section className="xl:padding-1 wide:padding-r paddig-b">HERO</section>
-      <section className="padding">POPULAR PRODUCTS</section>
-      <section className="padding">SUPER QUALITY</section>
-      <section className="padding-x py-10">SERVICES </section>
-      <section className="padding">SPECIAL OFFERS</section>
-      <section className="padding">CUSTOMER REVIEWS</section>
-      <section className="padding"></section>
-      <section className="padding">SPECIAL OFFERS</section>
+      <NavBar />
+      <section className="xl:padding-1 wide:padding-r paddig-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />{" "}
+      </section>
+      <section className="padding">
+        {" "}
+        <SpecialOffer />
+      </section>
+      <section className="padding bg-pale-blue">
+        {" "}
+        <CustomerReviews />
+      </section>
+      <section className="padding padding-x sm:py-32 py-16 w-full">
+        <Subscribe />{" "}
+      </section>
+      <section className="padding bg-black padding-x padding-t pb-8 text-white">
+        <Footer />
+      </section>
     </main>
   );
 };
